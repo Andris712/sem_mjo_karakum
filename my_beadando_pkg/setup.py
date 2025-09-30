c5d5bf80e2486c922ea29911e2aa498f02937f51
Andris712 <semseyandras12@gmail.com>
2025-09-30 15:23:42 +0200

    Update setup.py + package.xml; add talker_listener.launch.py

--- my_beadando_pkg/setup.py
+++ my_beadando_pkg/setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 package_name = 'my_beadando_pkg'
 
@@ -8,20 +8,23 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+         ['resource/' + package_name]),
+        ('share/' + package_name,
+         ['package.xml']),
+        ('share/' + package_name + '/launch',
+         ['launch/talker_listener.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ajr',
-    maintainer_email='ajr@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='aj',
+    maintainer_email='aj@todo.todo',
+    description='Kis beadandó ROS2 package.',
+    license='Apache-2.0',
     tests_require=['pytest'],
-entry_points={
-    'console_scripts': [
-        'talker = my_beadando_pkg.publisher_node:main',
-        'listener = my_beadando_pkg.subscriber_node:main',
-    ],
-},
+    entry_points={
+        'console_scripts': [
+            'talker = my_beadando_pkg.publisher_node:main',
+            'listener = my_beadando_pkg.subscriber_node:main',
+        ],
+    },
 )
