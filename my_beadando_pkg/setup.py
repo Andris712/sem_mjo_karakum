2672504cb8988c81eab60c96cbd42d284be025e9
Andris712 <semseyandras12@gmail.com>
2025-09-30 14:36:41 +0200

    Add my_beadando_pkg ROS2 package (talker + listener)

---
+++ my_beadando_pkg/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+package_name = 'my_beadando_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='aj',
+    maintainer_email='aj@todo.todo',
+    description='Kis beadandó ROS2 package',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = my_beadando_pkg.publisher_node:main',
+        ],
+    },
+)
+
+
